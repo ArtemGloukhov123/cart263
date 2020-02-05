92faf2c4209b5526ef6f04260a338c81a6dd9e47
Artem <>
2020-02-05 15:07:26 -0500

    P1: Added Sounds

--- project1/js/script.js
+++ project1/js/script.js
@@ -16,9 +16,21 @@ let $modules;
 
 let pay = 0;
 
+let clickSound = new Audio("/assets/sounds/click.mp3");
+let lightSound = new Audio("/assets/sounds/light.mp3");
+let boxSound = new Audio("/assets/sounds/box.mp3");
+let conveyorSound = new Audio("/assets/sounds/conveyor.mp3");
+let clockSound = new Audio("/assets/sounds/clock.mp3");
+
 $(document).ready(setup);
 
 function setup() {
+  lightSound.loop = true;
+  lightSound.play();
+
+  clockSound.loop = true;
+  clockSound.play();
+
   alert("Assemble the phone by placing the modules onto the motherboard, then place the phone into the box for shipment.")
   $("#buyFood").on("click", noMoney);
   $modules = $("#modules");
@@ -34,8 +46,9 @@ function setup() {
   $module.draggable();
   $phone.draggable();
   $phone.droppable({
-    drop: function(event,ui) {
+    drop: function(event, ui) {
       $phone.append(ui.draggable);
+      clickSound.play();
       ui.draggable.css({
         top: -170,
         left: 5
@@ -54,10 +67,12 @@ function moveConveyorBelt() {
     left: "350px"
   }, 4500, "linear");
   setTimeout(stopConveyorBelt, 4500);
+  conveyorSound.play();
 }
 
 function stopConveyorBelt() {
   $("#conveyorMoving").css("visibility", "hidden");
+  conveyorSound.pause();
 }
 
 function dropPhone() {
@@ -69,12 +84,14 @@ function dropPhone() {
   setTimeout(openBox, 1000);
   setTimeout(newPhone, 1000);
 
+  boxSound.play();
+
   pay += 0.01;
   $("#yourPay2").html(pay);
 }
 
 function openBox() {
-  if(pay > 0.04){
+  if (pay > 0.04) {
     pay = 0;
     $("#yourPay2").html(pay);
     alert("The shipment was lost in transit and you are not paid.");
@@ -85,28 +102,28 @@ function openBox() {
 }
 
 function newPhone() {
-    $phone.css("left", "-200px")
-    $phone.css("top", "300px")
-    $phone.css("visibility", "visible")
-    $module.css("visibility", "visible");
+  $phone.css("left", "-200px")
+  $phone.css("top", "300px")
+  $phone.css("visibility", "visible")
+  $module.css("visibility", "visible");
 
-    for(let i=0; i<$module.length; i++){
-      $module[i].remove();
-    }
+  for (let i = 0; i < $module.length; i++) {
+    $module[i].remove();
+  }
 
-    $phone.animate({
-      left: "300px"
-    }, 800,);
+  $phone.animate({
+    left: "300px"
+  }, 800, );
 }
 
 function createModules() {
-  for(let i = 0; i<4; i++) {
+  for (let i = 0; i < 4; i++) {
     let module = document.createElement("img");
     let moduleID = 'module' + i;
     module.setAttribute('id', moduleID);
-    module.setAttribute('class','module');
+    module.setAttribute('class', 'module');
     let imagenum = i + 1;
-    module.setAttribute('src','assets/images/module' + imagenum + '.png');
+    module.setAttribute('src', 'assets/images/module' + imagenum + '.png');
     $modules.append(module);
     $module = $('.module');
     $module.draggable();
