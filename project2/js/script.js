2c210c010ec4f1278f432fcc89706e9d6cd819dd
Artem <>
2020-03-04 20:33:40 -0500

    P2: Removed annyang

--- project2/js/script.js
+++ project2/js/script.js
@@ -33,23 +33,6 @@ $(document).ready(setup);
 function setup() {
   //have a start button so that the page has a user interaction before playing sound.
   $(".startButton").on("click", playGame);
-
-  //set up annyang to listen to certain phrases
-  if (annyang) {
-
-    //annyang will only react when it hears these phrases
-    var command = {
-      "I give up": giveUp,
-      "Say it again": repeat,
-      "I think it is *spokenGuess": handleSpokenGuess
-    };
-
-    //add the phrases to annyang
-    annyang.addCommands(command);
-
-    //now that it is set up, start annyang
-    annyang.start();
-  }
 }
 
 function playGame() {
