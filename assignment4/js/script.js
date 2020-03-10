7562e943e9034ccc78b52f74a74993f6bf93a227
Artem <>
2020-03-10 16:56:07 -0400

    A4: Added another JSON file for rabbits

--- assignment4/js/script.js
+++ assignment4/js/script.js
@@ -29,34 +29,42 @@ function loaded(data) {
   let randomCondiment = getRandomArrayElement(data.condiments);
   let randomCat = getRandomArrayElement(data.cats);
   let randomRoom = getRandomArrayElement(data.rooms);
+  let randomRabbit = getRandomArrayElement(data.rabbits);
 
   let verb = "is";
-  if(randomCondiment.charAt(randomCondiment.length - 1) === "s") {
+  if (randomCondiment.charAt(randomCondiment.length - 1) === "s") {
     verb = "are";
   }
 
-  let indefiniteArticle = "a";
-  if(randomCat.charAt(0) === "A") {
-    indefiniteArticle = "an";
-  }
-  if(randomCat.charAt(0) === "E") {
-    indefiniteArticle = "an";
-  }
-  if(randomCat.charAt(0) === "I") {
-    indefiniteArticle = "an";
-  }
-  if(randomCat.charAt(0) === "O") {
-    indefiniteArticle = "an";
-  }
-  if(randomCat.charAt(0) === "U") {
-    indefiniteArticle = "an";
-  }
+  let indefiniteArticle = getCorrectIndefiniteArticle(randomCat);
+  let indefiniteArticle2 = getCorrectIndefiniteArticle(randomRabbit);
 
-  createPhrase(randomCondiment, verb, indefiniteArticle, randomCat, randomRoom);
+  createPhrase(randomCondiment, verb, indefiniteArticle, randomCat, indefiniteArticle2, randomRabbit, randomRoom);
 }
 
 function notLoaded(request, text, error) {
   console.error(error);
+}
+
+function getCorrectIndefiniteArticle(element) {
+  let article = "a";
+
+  if (element.charAt(0) === "A") {
+    article = "an";
+  }
+  if (element.charAt(0) === "E") {
+    article = "an";
+  }
+  if (element.charAt(0) === "I") {
+    article = "an";
+  }
+  if (element.charAt(0) === "O") {
+    article = "an";
+  }
+  if (element.charAt(0) === "U") {
+    article = "an";
+  }
+  return article;
 }
 
 function getRandomArrayElement(array) {
@@ -64,8 +72,8 @@ function getRandomArrayElement(array) {
   return element;
 }
 
-function createPhrase(condiment, verb, indefiniteArticle, cat, room) {
-  let phrase = `${condiment} ${verb} like ${indefiniteArticle} ${cat} in the ${room}.`;
+function createPhrase(condiment, verb, indefiniteArticle, cat, indefiniteArticle2, rabbit, room) {
+  let phrase = `${condiment} ${verb} like ${indefiniteArticle} ${cat} cat chasing ${indefiniteArticle2} ${rabbit} rabbit in the ${room}.`;
 
   let $p = $("<p></p>");
   $p.addClass("phrase");
