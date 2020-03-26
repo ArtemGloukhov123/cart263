a23e47212dbf0cf1639696bc204be62903e355dd
Artem <>
2020-03-26 15:40:07 -0400

    P3: Added starter code and zoodle and faceplant logos

---
+++ project3/js/script.js
@@ -0,0 +1,17 @@
+"use strict";
+
+/********************************************************************
+
+Glorious Nation Internet Simulator
+Artem Gloukhov
+
+
+
+*********************************************************************/
+
+//run setup when document is loaded
+$(document).ready(setup);
+
+function setup() {
+
+}
