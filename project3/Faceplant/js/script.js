e2094e3f05bc0cf3574ff3cfa9d06b123399c38d
Artem <>
2020-03-26 17:58:36 -0400

    P3: Added basic Zoodle page

---
+++ project3/Faceplant/js/script.js
@@ -0,0 +1,18 @@
+"use strict";
+
+/********************************************************************
+
+Glorious Nation Internet Simulator
+Artem Gloukhov
+
+
+
+*********************************************************************/
+
+
+//run setup when document is loaded
+$(document).ready(setup);
+
+function setup() {
+
+}
