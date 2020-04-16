eb25775d4c4f5c48169ef51a46599fa1df47d197
Artem <>
2020-04-16 19:07:00 -0400

    P3: Small adjustments to desktop page

--- project3/Desktop/js/script.js
+++ project3/Desktop/js/script.js
@@ -27,27 +27,17 @@ function setup() {
 function openPicture() {
   // Dynamically create a div and store it in a variable. This is the div
   // we will turn into a dialog box. Set its title at the same time.
-  let $dialog = $(`<div></div>`).attr(`title`, `family.png`);
+  let $dialog = $(`<div id='dialogdiv'></div>`).attr(`title`, `family.png`);
 
   //add an image to the body of the dialog box
-  $dialog.append(`<img src="assets/images/family.png">`);
+  $dialog.append(`<img src="assets/images/family.png" id='familyimage'>`);
   // Finally, add the div to the page
   $('body').append($dialog);
 
   // Now we have our div on the page, transform it into a dialog with jQuery UI's
   // .dialog() method, supplying a number of options to configure it
   $dialog.dialog({
-    // The 'buttons' option lets us specify buttons to appear in the dialog as
-    // the properties of an object. The property name is used as the button text
-    // and the property contains a function that will be called when that button
-    // is clicked. Note how you can have quote marks around a property name (important
-    // if you want to include spaces for instance.)
-    // In this case both buttons just close the dialog
-    buttons: {
-      "Close": function() {
-        $(this).dialog(`close`);
-      }
-    },
+
     // The 'containment' option lets us specify where the dialog can go on the screen. 'body' means it will be
     // contained within the body tag, and can't be dragged out of it.
     containment: 'body',
