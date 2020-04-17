b03b4fb1f5da3b41d41f16eac1ea659185364c4c
Artem <>
2020-04-17 16:39:52 -0400

    P3: Made login page for VPN page

--- project3/Loginpage/js/script.js
+++ project3/Loginpage/js/script.js
@@ -9,12 +9,45 @@ Artem Gloukhov
 
 *********************************************************************/
 //login info for this page
-let login = "JinpingXi";
-let password = "GreatFirewall";
+let correctLogin = "JinpingXi";
+let correctPassword = "GreatFirewall";
 
 //run setup when document is loaded
 $(document).ready(setup);
 
 function setup() {
+  $('#signin').on('click', checkLoginInfo);
+}
 
+//checks that the login and password are correct
+function checkLoginInfo() {
+  //get value entered into input fields
+  let login = document.getElementById('login').value;
+  let password = document.getElementById('password').value;
+
+  //check that they are correct
+  if(login === correctLogin){
+    if(password === correctPassword){
+      window.location.href = "../VPN/VPN.html"
+    } else {
+      magicWord();
+    }
+  } else {
+    magicWord();
+  }
+
+
+}
+
+function magicWord(){
+  $('#magicword').css('visibility', 'visible');
+
+  responsiveVoice.speak("ah ah aaah. You didn't say the magic word", "UK English Male", {
+      rate: 0.9,
+      pitch: 0.9
+    });
+
+    setTimeout(function(){
+      $('#magicword').css('visibility', 'hidden');
+    }, 4000);
 }
