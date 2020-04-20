29aff1489ac7e9a135289f22f5ac48f6165d8a58
Artem <>
2020-04-19 23:07:36 -0400

    P3: Added dialog box shown once on desktop suggesting full screen

--- project3/Desktop/js/script.js
+++ project3/Desktop/js/script.js
@@ -10,6 +10,14 @@ Artem Gloukhov
 $(document).ready(setup);
 
 function setup() {
+  //alert the player that the game is best played in full screen
+  //only do this once, as the player doesn't need a reminder every time they return to the desktop
+  //Done this way instead of forcing fullscreen as forcing would be required on every single page
+  let checkIfIntroDialogWasRead = localStorage.getItem("introDialogClosed");
+
+  if(checkIfIntroDialogWasRead !== "true"){
+    dialogBox("This interactive experience is best played through full screen mode.");
+  }
 
   //go to internet page when internet icon is clicked
   $("#internetdiv").on("click", function() {
@@ -26,7 +34,7 @@ function setup() {
 
 function openPicture() {
   //code taken from Pippin Barr's "Endless Dialogs" game
-  
+
   //open a dialog box with the family picture inside of it
   //start with a div
   let $dialog = $(`<div id='dialogdiv'></div>`).attr(`title`, `family.png`);
@@ -43,6 +51,42 @@ function openPicture() {
     //contained within the body of the document
     containment: 'body',
     width: 600
+  });
+
+  $dialog.parent().offset({
+    top: 0.5 * ($(window).height() - $dialog.parent().height()),
+    left: 0.5 * ($(window).width() - $dialog.parent().width())
+  });
+}
+
+//create a dialog box
+function dialogBox(text) {
+
+  //create a div to be turn into a dialog box
+  let $dialog = $(`<div id='dialogdiv'></div>`).attr(`title`, `Alert`);
+
+  //add text to the div
+  $dialog.append(`<p>${text}</p>`);
+
+  //Add the div to the page
+  $('body').append($dialog);
+
+  //turn the div into a dialog box
+  $dialog.dialog({
+
+    //add a button to take the user to the ending screen
+    //code found at https://www.w3schools.com/howto/howto_js_fullscreen.asp
+    buttons: {
+      "Close": function() {
+        $(this).dialog(`close`);
+        localStorage.setItem("introDialogClosed", "true");
+      }
+
+    },
+
+    //contain within body
+    containment: 'body',
+    width: 500
   });
 
   $dialog.parent().offset({
